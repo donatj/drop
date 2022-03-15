d81410665922a8cd2757d08cd42692b92c545afa
Jesse Donat <donatj@gmail.com>
2022-03-14 21:48:09 -0500

    More testing

--- test/DropTest.php
+++ test/DropTest.php
@@ -8,15 +8,61 @@ class DropTest extends TestCase {
 
 	/**
 	 * @runInSeparateProcess
-	 * @testWith
-	 *    [ true ]
-	 *    [ false ]
+	 * @dataProvider boolProvider
+	 * @param bool $bool
+	 * @return void
+	 */
+	public function test_see( bool $bool ) : void {
+		define('IS_CLI', $bool);
+
+		ob_start();
+		see([ 1, 2, 3 ]);
+		$out = ob_get_clean();
+
+		$content = <<<OUT
+
+
+||||||||||||||||||||| Arg No. 0  |||||||||||||||||||||||||||||||||||||||||||
+
+
+
+Array
+(
+    [0] => 1
+    [1] => 2
+    [2] => 3
+)
+
+
+
+
+||||||||||||||||||||| EOF ||||||||||||||||||||||||||||||||||||||||||||||||||
+
+
+OUT;
+
+
+		if( $bool ) {
+			$this->assertSame($content, $out);
+		} else {
+			$this->assertSame("<pre>{$content}</pre>", $out);
+		}
+	}
+
+	/**
+	 * @runInSeparateProcess
+	 * @dataProvider boolProvider
 	 * @param bool $bool
 	 * @return void
 	 */
 	public function test_is_cli( bool $bool ) : void {
 		define('IS_CLI', $bool);
 		$this->assertSame($bool, is_cli());
+	}
+
+	public function boolProvider() : \Generator {
+		yield [ true ];
+		yield [ false ];
 	}
 
 }
